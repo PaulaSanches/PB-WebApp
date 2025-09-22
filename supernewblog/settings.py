482db5ae77b04e51e43931433c27b6e71aeb5d44
Paula Barrientos <80291570+PaulaSanches@users.noreply.github.com>
2025-09-23 11:51:40 +1200

    Update settings.py

--- supernewblog/settings.py
+++ supernewblog/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4k*noir2d57-2c^wtjy%yf6azn+dnqy(f-$vvvpm)8aiel$-oz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['pb-webapp-aphacjdpehg0awdq.newzealandnorth-01.azurewebsites.net']
 
 
 # Application definition
